755349e7175c184d14d35e5fbef02381c21831ad
Harsahib Singh Matharoo <harsahib01@hotmail.com>
2023-08-15 17:42:47 -0400

    Barcode App Uploaded

---
+++ QR Code Scanner/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace QR_Code_Scanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.HelpButton = new System.Windows.Forms.Button();
+            this.buttonEnhanceQuality = new System.Windows.Forms.Button();
+            this.button_close = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(41, 73);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(466, 364);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(565, 73);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(439, 376);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(905, 12);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(565, 15);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(331, 24);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(565, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Output:";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(215, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(99, 39);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "Camera";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(116, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(378, 38);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "HM\'s Bar Code Scanner ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Sitka Text Semibold", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(12, 473);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(616, 28);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.Text = "Say \"Scan\" to turn on Camera. After turned on, say \"Close\" to turn off.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(118, 441);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 29);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Voice Commands:";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(63, 594);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(923, 77);
+            this.richTextBox2.TabIndex = 11;
+            this.richTextBox2.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(406, 561);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(264, 29);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Text to Speech Output";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // HelpButton
+            // 
+            this.HelpButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HelpButton.Location = new System.Drawing.Point(624, 708);
+            this.HelpButton.Name = "HelpButton";
+            this.HelpButton.Size = new System.Drawing.Size(362, 66);
+            this.HelpButton.TabIndex = 13;
+            this.HelpButton.Text = "Help";
+            this.HelpButton.UseVisualStyleBackColor = true;
+            this.HelpButton.Click += new System.EventHandler(this.HelpButton_Click_1);
+            // 
+            // buttonEnhanceQuality
+            // 
+            this.buttonEnhanceQuality.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEnhanceQuality.Location = new System.Drawing.Point(63, 708);
+            this.buttonEnhanceQuality.Name = "buttonEnhanceQuality";
+            this.buttonEnhanceQuality.Size = new System.Drawing.Size(362, 66);
+            this.buttonEnhanceQuality.TabIndex = 14;
+            this.buttonEnhanceQuality.Text = "Enhance Camera Quality";
+            this.buttonEnhanceQuality.UseVisualStyleBackColor = true;
+            this.buttonEnhanceQuality.Click += new System.EventHandler(this.buttonEnhanceQuality_Click);
+            // 
+            // button_close
+            // 
+            this.button_close.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_close.Location = new System.Drawing.Point(779, 473);
+            this.button_close.Name = "button_close";
+            this.button_close.Size = new System.Drawing.Size(225, 62);
+            this.button_close.TabIndex = 15;
+            this.button_close.Text = "Close Camera";
+            this.button_close.UseVisualStyleBackColor = true;
+            this.button_close.Click += new System.EventHandler(this.button_close_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(1083, 817);
+            this.Controls.Add(this.button_close);
+            this.Controls.Add(this.buttonEnhanceQuality);
+            this.Controls.Add(this.HelpButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "QR Code Scanner - Hash\'J";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button HelpButton;
+        private System.Windows.Forms.Button buttonEnhanceQuality;
+        private System.Windows.Forms.Button button_close;
+    }
+}
+
---
+++ QR Code Scanner/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using ZXing;
+using System.Media;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Speech;
+using System.Speech.Recognition;
+
+namespace QR_Code_Scanner
+{
+    public partial class Form1 : Form
+    {
+        private bool isScanning = false;
+        private SoundPlayer successSoundPlayer = new SoundPlayer(Path.Combine(Application.StartupPath, "success.wav"));
+        private SpeechRecognitionEngine speechRecognizer;
+        private bool isCameraEnhanced = false;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeSpeechRecognition();
+        }
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice videoCaptureDevice;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterInfo in filterInfoCollection)
+            {
+                comboBox1.Items.Add(filterInfo.Name);  
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            StartCamera();
+        }
+        
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs) 
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = bitmap;
+        }
+        private void InitializeSpeechRecognition()
+        {
+            speechRecognizer = new SpeechRecognitionEngine();
+
+            // Create a free-form dictation grammar to recognize any spoken words
+            Grammar dictationGrammar = new DictationGrammar();
+            dictationGrammar.Name = "DictationGrammar";
+
+            // Create a grammar for the "scan" command with a higher priority
+            Choices scanChoices = new Choices();
+            scanChoices.Add("scan");
+
+            GrammarBuilder scanGrammarBuilder = new GrammarBuilder(scanChoices);
+
+            Grammar scanGrammar = new Grammar(scanGrammarBuilder);
+            scanGrammar.Name = "ScanGrammar";
+            scanGrammar.Priority = 1; // Emphasize "scan"
+
+            // Create a grammar for the "close" command with a higher priority
+            Choices closeChoices = new Choices();
+            closeChoices.Add("close");
+
+            GrammarBuilder closeGrammarBuilder = new GrammarBuilder(closeChoices);
+
+            Grammar closeGrammar = new Grammar(closeGrammarBuilder);
+            closeGrammar.Name = "CloseGrammar";
+            closeGrammar.Priority = 1; // Emphasize "close"
+
+            // Load all grammars
+            speechRecognizer.LoadGrammar(dictationGrammar);
+            speechRecognizer.LoadGrammar(scanGrammar);
+            speechRecognizer.LoadGrammar(closeGrammar);
+
+            speechRecognizer.SpeechRecognized += SpeechRecognizer_SpeechRecognized;
+            speechRecognizer.SetInputToDefaultAudioDevice();
+            speechRecognizer.RecognizeAsync(RecognizeMode.Multiple);
+        }
+        private void SpeechRecognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string recognizedText = e.Result.Text.ToLower();
+
+            if (recognizedText == "scan")
+            {
+                // Start camera when "scan" command is recognized
+                StartCamera();
+            }
+            else if (recognizedText == "close")
+            {
+                // Close camera when "close" command is recognized
+                CloseCamera();
+                MessageBox.Show("Camera closed.", "Camera Status", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+            richTextBox2.AppendText("You said: " + recognizedText + Environment.NewLine);
+            richTextBox2.ScrollToCaret();
+        }
+
+        private void CloseCamera()
+        {
+            if (isScanning)
+            {
+                if (videoCaptureDevice != null && videoCaptureDevice.IsRunning)
+                {
+                    videoCaptureDevice.SignalToStop();
+                    videoCaptureDevice.WaitForStop();
+                    isScanning = false;
+                    pictureBox1.Image = null;
+                    timer1.Stop();
+                }
+            }
+        }
+
+        private void StartCamera()
+        {
+            if (!isScanning)
+            {
+                isScanning = true;
+
+                videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[comboBox1.SelectedIndex].MonikerString);
+                videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+
+                if (isCameraEnhanced)
+                {
+                    videoCaptureDevice.VideoResolution = videoCaptureDevice.VideoCapabilities[0];
+                }
+                videoCaptureDevice.Start();
+                timer1.Start();
+            }
+        }
+
+
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoCaptureDevice.IsRunning)
+            {
+                videoCaptureDevice.Stop();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                BarcodeReader redaeRedocraB = new BarcodeReader();
+                Result result = redaeRedocraB.Decode((Bitmap)pictureBox1.Image);
+                if (result != null)
+                {
+
+                    string scannedText = result.Text;
+
+                    // Check if the scanned content is a URL
+                    if (Uri.IsWellFormedUriString(scannedText, UriKind.Absolute))
+                    {
+                        // Open the URL in the default web browser
+                        System.Diagnostics.Process.Start(scannedText);
+                    }
+
+                    successSoundPlayer.Play();
+                    richTextBox1.Text = result.ToString();
+                    
+             
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HelpButton_Click_1(object sender, EventArgs e)
+        {
+            // Create and show a new form for the help guide
+            Form helpForm = new Form();
+            helpForm.Text = "Quick Help Guide";
+            helpForm.Width = 400;
+            helpForm.Height = 400;
+
+            // Add labels or textboxes to display help content
+            Label label1 = new Label();
+            label1.Text = "Bar Code Scanner App!";
+            label1.Location = new Point(100, 10);
+            helpForm.Controls.Add(label1);
+
+            // Add a paragraph explaining the app's features
+            TextBox textBox = new TextBox();
+            textBox.Multiline = true;
+            textBox.Text = "The Bar Code Scanner App allows you to scan QR or bar codes using your device's camera. "
+                         + "Simply click the 'Scan' button or use voice commands to activate the camera and point it at a QR code. "
+                         + "Once the code is detected, the app will output the scanned content and provide "
+                         + "options to open URLs, play sound effects, and more.";
+            textBox.Location = new Point(10, 40);
+            textBox.Size = new Size(360, 240);
+            textBox.ReadOnly = true;
+            helpForm.Controls.Add(textBox);
+
+            // Show the help guide form
+            helpForm.ShowDialog();
+        }
+
+        private void buttonEnhanceQuality_Click(object sender, EventArgs e)
+        {
+            if (!isCameraEnhanced)
+            {
+                // Adjust camera settings for enhanced quality
+                if (videoCaptureDevice != null && videoCaptureDevice.IsRunning)
+                {
+                    videoCaptureDevice.Stop();
+                    videoCaptureDevice.VideoResolution = videoCaptureDevice.VideoCapabilities[0];
+                    videoCaptureDevice.Start();
+                }
+
+                isCameraEnhanced = true;
+                buttonEnhanceQuality.Text = "Restore Quality";
+            }
+            else
+            {
+                // Restore default camera settings
+                if (videoCaptureDevice != null && videoCaptureDevice.IsRunning)
+                {
+                    videoCaptureDevice.Stop();
+                    videoCaptureDevice.VideoResolution = videoCaptureDevice.VideoCapabilities[videoCaptureDevice.VideoCapabilities.Length - 1];
+                    videoCaptureDevice.Start();
+                }
+
+                isCameraEnhanced = false;
+                buttonEnhanceQuality.Text = "Enhance Quality";
+            }
+        }
+
+        private void button_close_Click(object sender, EventArgs e)
+        {
+            CloseCamera();
+        }
+    }
+}
